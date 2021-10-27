61c866cc55e131b4ec62d879e0fb0d2aa5d66523
Guillermo Castro <guillermo.castro@mail.escuelaing.edu.co>
2021-10-27 18:52:24 -0500

    first create link

---
+++ src/components/generatelink/generatelink.jsx
@@ -0,0 +1,24 @@
+import Navbar from "../Navbar"
+import { Link} from 'react-router-dom';
+
+function generatelink() {  
+    return (
+            <div id="generate">
+                <Navbar />
+                <div id="generatelink">
+                    <br />
+                    <h1>Generate Link</h1>
+                    <br />
+                    <form id="formsign">
+                        <div class="mb-3">
+                            <input type="text" placeholder="Link" class="form-control" id="Link" />
+                        </div>
+                        <button type="submit" id="buttonsign" class="btn btn-primary">Generate Link</button>
+                        <br />
+                    </form>
+                </div>
+            </div>
+    );
+}
+
+export default generatelink;
